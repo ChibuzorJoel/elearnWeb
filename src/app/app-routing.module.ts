159a40e1b77978fa0eedd1671b8132211da8a483
yungjoel <chibuzoronyedikachi72@gmail.com>
2025-01-04 08:49:47 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { CoursesComponent } from './pages/courses/courses.component';
+import { NewsComponent } from './pages/news/news.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'courses', component: CoursesComponent},
+  { path: 'news', component: NewsComponent},
+  { path: 'contact', component: ContactComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { CoursesComponent } from './pages/courses/courses.component';
+import { NewsComponent } from './pages/news/news.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { InstructorsComponent } from './pages/instructors/instructors.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    CoursesComponent,
+    NewsComponent,
+    ContactComponent,
+    BlogComponent,
+    InstructorsComponent
+    
   ],
   imports: [
     BrowserModule,
